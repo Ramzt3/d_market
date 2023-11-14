e12b3f2dcb87b690f75fc5c6b03a555538ca10f8
siliphong <it@BOOKNEAs-Mac-mini.local>
2023-11-14 17:00:52 +0700

    add authentication

--- item/models.py
+++ item/models.py
@@ -24,7 +24,7 @@ class Item(models.Model):
     image = models.ImageField(upload_to="item_images", blank=True, null=True)
     is_sold = models.BooleanField(default=False)
     created_by = models.ForeignKey(User, related_name="items", on_delete=models.CASCADE)
-    craeted_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name

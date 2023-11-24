7843ad7f9047e20b48541313522b7fd2d1f1d317
siliphong-mac <it@BOOKNEAs-Mac-mini.local>
2023-11-24 15:49:26 +0700

    add browse items

--- item/views.py
+++ item/views.py
@@ -1,8 +1,27 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import get_object_or_404, render, redirect
 
-from .forms import NewItemForm
-from .models import Item
+from .forms import NewItemForm, EditItemForm
+from .models import Item, Category
+
+def items(request):
+    query = request.GET.get('query', '')
+    category_id = request.GET.get('category', 0)
+    categories = Category.objects.all()
+    items = Item.objects.filter(is_sold=False)
+
+    if category_id:
+        items =items.filter(category_id=category_id)
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    return render(request, 'item/items.html', {
+        'items': items,
+        'categories': categories,
+        'query': query,
+        'category_id': int(category_id),
+    })
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
@@ -30,4 +49,30 @@ def new(request):
         'form': form,
         'title': 'New item',
     })
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit item',
+    })
+
+@login_required
+def delete(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    item.delete()
+
+    return redirect('dashboard:index')
 
